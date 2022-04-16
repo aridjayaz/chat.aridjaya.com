5d8d719aa5c2cf311158cc102a5ea6fdeee58d51
irawanria85 <102246214+infojandamuda@users.noreply.github.com>
2022-04-16 19:21:06 +0700

    w

--- public/ramadhan/js/min.js
+++ public/ramadhan/js/min.js
@@ -1,10 +1,15 @@
 // title and thumbnail
-thumbnail();
+scriptRamadhan()
 
-function thumbnail() {
+function scriptRamadhan() {
+
+    document.getElementById("m1").innerHTML = "<img class='flower' src='./img/pink.png' /><img class='smile' src='./img/smile.png' /><img class='flower' src='./img/blue.png' /><img class='heart' src='./img/heart.png' /><img class='flower' src='./img/pink.png' /><img class='smile' src='./img/smile.png' /><img class='flower' src='./img/blue.png' /><img class='heart' src='./img/heart.png' /><img class='flower' src='./img/pink.png' /><img class='smile' src='./img/smile.png' /><img class='flower' src='./img/blue.png' /><img class='heart' src='./img/heart.png' /><img class='flower' src='./img/pink.png' /><img class='smile' src='./img/smile.png' /><img class='flower' src='./img/blue.png' /><img class='heart' src='./img/heart.png' /><img class='flower' src='./img/pink.png' /><img class='smile' src='./img/smile.png' /><img class='flower' src='./img/blue.png' /><img class='heart' src='./img/heart.png' /><img class='flower' src='./img/pink.png' /><img class='smile' src='./img/smile.png' /><img class='flower' src='./img/blue.png' /><img class='heart' src='./img/heart.png' /><img class='flower' src='./img/pink.png' />";
+    document.getElementById("m2").innerHTML = "<img class='flower' src='./img/pink.png' /><img class='smile' src='./img/smile.png' /><img class='flower' src='./img/blue.png' /><img class='heart' src='./img/heart.png' /><img class='flower' src='./img/pink.png' /><img class='smile' src='./img/smile.png' /><img class='flower' src='./img/blue.png' /><img class='heart' src='./img/heart.png' /><img class='flower' src='./img/pink.png' /><img class='smile' src='./img/smile.png' /><img class='flower' src='./img/blue.png' /><img class='heart' src='./img/heart.png' /><img class='flower' src='./img/pink.png' /><img class='smile' src='./img/smile.png' /><img class='flower' src='./img/blue.png' /><img class='heart' src='./img/heart.png' /><img class='flower' src='./img/pink.png' /><img class='smile' src='./img/smile.png' /><img class='flower' src='./img/blue.png' /><img class='heart' src='./img/heart.png' /><img class='flower' src='./img/pink.png' /><img class='smile' src='./img/smile.png' /><img class='flower' src='./img/blue.png' /><img class='heart' src='./img/heart.png' /><img class='flower' src='./img/pink.png' />";
+    document.getElementById("preloader-box").innerHTML = "<div>H</div><div>A</div><div>P</div><div>P</div><div>Y</div><div>-</div><div>R</div><div>A</div><div>M</div><div>A</div><div>D</div><div>H</div><div>A</div><div>N</div>";
     document.getElementById("thumb").innerHTML = images[img];
     document.getElementById("text").innerHTML = Quotation[text];
     document.getElementById("child").innerHTML = child[family];
+
 }
 
 /* img onclick */
@@ -17,5 +22,5 @@ $(document).ready(function () {
 
 // share
 document.getElementById("share").onclick = function () {
-    window.open("whatsapp://send?text=*🌸 Happy Ramadhan Kareem 🌸*%0Ahttps://chat.aridjaya.com/ramadhan  %0A%0AMarhaban ya Ramadhan %0A%0AKlik tulisan biru ini dan ketik namamu, maka kamu akan tahu ada keajaiban didalamnya 👀%0A👇👇👇%0Ahttps://chat.aridjaya.com/ramadhan%0A%0A_gak di sangka, ternyata...😁👍_")
+    window.open("whatsapp://send?text=*🌸 Happy Ramadhan Kareem 🌸*%0Ahttps://chat.aridjaya.com/ramadhan  %0A%0A*Marhaban ya Ramadhan* %0A%0A_Klik tulisan biru ini dan ketik namamu, maka kamu akan tahu ada keajaiban didalamnya_ 👀%0A👇👇👇%0A%0A*Ramadhan Mubarak*%0Ahttps://chat.aridjaya.com/ramadhan%0A%0A_gak di sangka, ternyata.._")
 };
