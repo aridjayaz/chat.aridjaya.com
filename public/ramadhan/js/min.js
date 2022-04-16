695be5dac38ca96a98010fd4dbeb261f821d3cba
irawanria85 <102246214+infojandamuda@users.noreply.github.com>
2022-04-16 15:33:09 +0700

    wa

---
+++ public/ramadhan/js/min.js
@@ -0,0 +1,7 @@
+// title and thumbnail
+thumbnail();
+
+function thumbnail() {
+    document.getElementById("thumb").innerHTML = images[img];
+    document.getElementById("text").innerHTML = Quotation[text];
+}
